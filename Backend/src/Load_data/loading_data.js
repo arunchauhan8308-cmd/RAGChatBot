6afacc58346f62596f85822c6caf715b7f1a3b85
arunchauhan8308-cmd <arunchauhan8308@gmail.com>
2026-09-11 08:48:13 +0530

    updated

--- Backend/src/Load_data/loading_data.js
+++ Backend/src/Load_data/loading_data.js
@@ -1,15 +1,44 @@
-const fs = require('fs');
-const pdf_parse = require('pdf-parse');
+const fs = require('fs/promises');
+const path = require('path');
+const { PDFLoader } = require('@langchain/community/document_loaders/fs/pdf');
 
 const directory = '../../Data';
 
-const DirectoryLoader = async (directory) => {
-    const files = await fs.readFileSync(directory);
-    console.log(files.length)   
-}
+const LoadTheDirectory = async (directory) => {
+    // read the all files in the given directory
+    const files = await fs.readdir(directory)
 
-exports.loadTheData = async (directory) => {
-    console.log("Abhishek Raj");
+    // read the .pdf file in the all files
+    const pdfFiles = files.filter(file => path.extname(file) == '.pdf');
+
+    // 
+    // return the founded pdf files 
+    return pdfFiles
 };
 
-DirectoryLoader(directory);
+const pdfFileRead = async (files) => {
+
+    let data = [];
+    // read the files
+    for (let file of files) {
+        // const readPdfFileData = await fs.readFile(file);
+        const DataBuffer = new PDFLoader(file)
+        const pdfData = await DataBuffer.load();
+
+        data.push(...pdfData);
+    };
+    
+}
+
+const DirectoryLoader = async () => {
+    const dir = await LoadTheDirectory(directory);
+    const pdfLoader = await pdfFileRead(dir)
+    // console.log(pdfLoader.length)
+}
+
+DirectoryLoader();
+
+module.exports = {
+    DirectoryLoader
+}
+
